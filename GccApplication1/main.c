be13f4402cf5b1ff666ce510e05c81ff46f48886
CaioBaska <caiofigueiredosilva2001@hotmail.com>
2021-08-13 21:03:23 -0300

    TP1

---
+++ GccApplication1/main.c
@@ -0,0 +1,19 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 13/08/2021 20:33:20
+ * Author : caiof
+ */ 
+
+#include <avr/io.h>
+extern void papel();
+
+
+int main(void)
+{
+    papel();
+    while (1) 
+    {
+    }
+}
+
